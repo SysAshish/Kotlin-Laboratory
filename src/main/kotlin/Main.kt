15547442d29ce84e97d1a72cfd76a9db3173c037
SysAshish <ashishashara007@gmail.com>
2026-08-16 18:29:14 +0530

    Main method or starting point of the program

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,4 @@
 fun main() {
-    // Even or Odd
-    print("Enter number: ")
-    val num = readln().toInt()
-    val output = when {
-        num % 2 == 0 -> "The Number is even!"
-        else -> "The Number is odd!"
-    }
-    print(output)
+    // Normal Structure
+    println("Hello Kotlin")
 }
